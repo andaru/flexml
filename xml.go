b530c0f62f9f9541503446bf2a1594f8fb875f66
Andrew Fort <afort@choqolat.org>
2017-08-11 19:39:05 -0700

    golint: fix "xml" package comment
    Eliminate golint warning:
    
      xml.go:5:1:warning: package comment should be of the form
                 "Package flexml ..." (golint)

--- xml.go
+++ xml.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// package flexml implements a simple XML 1.0 parser that
+// Package flexml implements a simple XML 1.0 parser that
 // understands XML name spaces.
 package flexml
 
